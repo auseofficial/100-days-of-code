11cb32a5e34853d52ae8979b78175f26160f6c61
Akib Us Suny Eshan <eshan.r029@gmail.com>
2025-03-20 12:53:10 +0600

    day4

--- day3.py
+++ day3.py
@@ -1,28 +1,40 @@
-print("welcome to day 3")
+# print("welcome to day 3")
+#
+# # if_else
+#
+# height = int(input("What is your height? "))
+# if height < 6:
+#     print(f"Here is your height {height}")
+# else:
+#     print("Invalid height ")
+#
+# number_to_check = int(input("What is your number you want to check ?"))
+#
+# if number_to_check % 2 == 0:
+#     print("This is an even number ")
+# else:
+#     print("This is an odd number ")
+#
+# # Nested if_else
+# height = int(input("What is your height in cm ?"))
+# if height >= 120:
+#     print("You can ride boat")
+#     age = int(input("Write your age"))
+#     if age >=18:
+#         print("Okay you are eligible for a boat ride")
+#     else:
+#         print("Not eligible ")
+# else:
+#     print("Sorry hopefully you will be able to ride boat later")
 
-# if_else
+print(r''' 
+ _               _           _       _              
+| |             | |         (_)     | |              
+| |__   __ _  __| |_ __ ___  _ _ __ | |_ ___  _ __  
+| '_ \ / _` |/ _` | '_ ` _ \| | '_ \| __/ _ \| '_ \ 
+| |_) | (_| | (_| | | | | | | | | | | || (_) | | | |
+|_.__/ \__,_|\__,_|_| |_| |_|_|_| |_|\__\___/|_| |_|
+''')
 
-height = int(input("What is your height? "))
-if height < 6:
-    print(f"Here is your height {height}")
-else:
-    print("Invalid height ")
+print('\'You "are" Eshan.')
 
-number_to_check = int(input("What is your number you want to check ?"))
-
-if number_to_check % 2 == 0:
-    print("This is an even number ")
-else:
-    print("This is an odd number ")
-
-# Nested if_else
-height = int(input("What is your height in cm ?"))
-if height >= 120:
-    print("You can ride boat")
-    age = int(input("Write your age"))
-    if age >=18:
-        print("Okay you are eligible for a boat ride")
-    else:
-        print("Not eligible ")
-else:
-    print("Sorry hopefully you will be able to ride boat later")
